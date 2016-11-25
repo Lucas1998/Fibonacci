d8db57027e18c9ef97cdb7ba2b0282933467e44e
Lucas1998 <lucas98cm@hotmail.es>
2016-11-25 12:01:46 +0100

    n

--- Fibonacci/Funcion/Funciones.py
+++ Fibonacci/Funcion/Funciones.py
@@ -1,7 +1,11 @@
-f0=0
-f1=1
-def FuncionFibo (fn):
-    fn=fn-1+fn-2
-    return fn
-fn=int(input("introducir numero"))
-print("La Funcion de Fibonacci es %d" %fn)
+def FuncionFibo (num):
+    if num==1:
+       return 1
+    if num==0:
+       return 0
+    else:
+        return FuncionFibo(num-1) + FuncionFibo(num-2)     
+  
+if __name__=="__main__":
+    num=int(input("introducir numero"))
+    print("La Funcion de Fibonacci es %d" %num)
